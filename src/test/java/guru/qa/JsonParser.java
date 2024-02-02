d3923cca188c3169103db4339655759c26c3cf7d
AndreyTsGit <andreytseporinda@gmail.com>
2024-02-02 16:47:41 +0300

    add Json

--- src/test/java/guru/qa/JsonParser.java
+++ src/test/java/guru/qa/JsonParser.java
@@ -16,21 +16,21 @@ public class JsonParser {
 
     public static <ObjectMapper, JsonNode> void parseJsonFile(String jsonFilePath) {
         try {
-            // Создаем объект ObjectMapper, который будет использоваться для разбора JSON
+
             ObjectMapper objectMapper = new ObjectMapper();
 
-            // Читаем JSON-файл и получаем корневой узел (root node)
+
             JsonNode rootNode = objectMapper.readTree(new File(jsonFilePath));
 
-            // Получаем массив "people"
+
             JsonNode peopleArray = rootNode.get("people");
 
-            // Итерируем по элементам массива "people"
+
             Iterator<JsonNode> peopleIterator = peopleArray.iterator();
             while (peopleIterator.hasNext()) {
                 JsonNode personNode = peopleIterator.next();
 
-                // Извлекаем значения полей
+
                 String firstName = personNode.get("firstName").asText();
                 String lastName = personNode.get("lastName").asText();
                 String gender = personNode.get("gender").asText();
